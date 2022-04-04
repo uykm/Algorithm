4b0bca41f8080d7cdd4c9fa375517ba6a43977eb
Minkyu Shin <digi1k2001@gmail.com>
2022-04-04 12:05:24 +0900

    20220404

---
+++ Algorithm/Stack3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main(void)
+{
+	stack<char> st;
+
+	int T;
+	cin >> T;
+	cin.ignore();		// getline 으로 입력받으려면, cin 사용 후 '\n'가 입력버퍼에 남아있어 이렇게 버퍼를 비워주어야 함.
+						// getline 은 cin 처럼 엔터를 무시하지 않고 하나의 입력으로 받아들이기 때문임.
+	for (int i = 0; i < T; i++)
+	{
+		string arr;
+		getline(cin, arr);		// getline() 은 string 형을 받을 경우 사용, 공백이 포함된 문자열을 입력받기 편리함.
+								// getline(입력 스트림, string 객체, 구분자(생략할 경우, '\n'로 인식)
+								// cin.getline() 은 char* 형 문자열을 받는 경우에 사용
+		arr += ' ';		// 문장의 끝 ' ' 로 변환
+
+		for (auto key : arr)	// arr 의 요소를 반복하여 변수 key에 할당하는 것
+								// 이때, key 는 배열 arr 과 같은 자료형이어야 하기 때문에, auto 키워드를 활용
+		{
+			if (key == ' ')		// 문장의 중간이나 끝인 경우
+			{
+				while (!st.empty())
+				{
+					cout << st.top();
+					st.pop();
+				}
+				cout << key;		// 띄어쓰기 출력
+			}
+			else 
+				st.push(key);
+		}
+		cout << '\n';
+	}
+	return 0;
+}
